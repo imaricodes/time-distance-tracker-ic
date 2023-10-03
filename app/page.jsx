a9c13c330b0a7a988b1b705aefc090f38dda27a4
Imari Childress <16360390+imaricodes@users.noreply.github.com>
2023-10-03 16:41:48 -0400

    Remove index page code

--- app/page.jsx
+++ app/page.jsx
@@ -8,16 +8,6 @@ import { useState, useEffect } from "react";
 export default function Home() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("api/get-users", { cache: "no-store" });
-      const data = await res.json();
-      console.log("I am Data");
-      console.log(data);
-      setUsers(data);
-    }
-    fetchData();
-  }, []);
 
   return (
     <main className="flex flex-col items-center min-h-screen">
@@ -33,3 +23,38 @@ export default function Home() {
     </main>
   );
 }
+// "use client";
+// import { useState, useEffect } from "react";
+
+// // export const metadata = {
+// //   title: "Home Page",
+// // };
+
+// export default function Home() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const res = await fetch("api/get-users", { cache: "no-store" });
+//       const data = await res.json();
+//       console.log("I am Data");
+//       console.log(data);
+//       setUsers(data);
+//     }
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <main className="flex flex-col items-center min-h-screen">
+//       <h1 className="text-4xl font-bold text-primary mb-4 mt-12">
+//         Simply Track Miles
+//       </h1>
+//       <p className="text-lg border border-primary-light p-4 rounded-lg max-w-screen-md text-center">
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+//         molestiae odit odio tempora assumenda dignissimos numquam debitis quos
+//         fugiat repellendus, nesciunt fugit exercitationem nemo rem neque quae
+//         ratione, qui laborum?
+//       </p>
+//     </main>
+//   );
+// }
